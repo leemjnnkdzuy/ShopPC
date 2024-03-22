4eaf97c9a3d772410f8de05cca2b4d3e84d9cffa
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-03-22 14:47:25 +0700

    test

---
+++ root.js
@@ -0,0 +1,16 @@
+const ul = React.createElement(
+    'ul',
+    null,
+    React.createElement(
+        'li', 
+        null,
+        'Javascript'
+    ),
+    React.createElement(
+        'li',
+        null,
+        'ReactJS'
+    ),
+)
+
+ReactDOM.render(ul, document.getElementById('root'))
